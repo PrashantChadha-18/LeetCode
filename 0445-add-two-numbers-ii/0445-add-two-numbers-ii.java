d5b37f18da844232189672943d1450724de7075e
PrashantChadha-18 <prashantchadha58@gmail.com>
2026-07-21 13:34:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -12,29 +12,29 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy=new ListNode(0);
         ListNode curr=dummy;
-        Stack<ListNode> stk=new Stack<>();
-        Stack<ListNode> s=new Stack<>();
+        Stack<ListNode> s1=new Stack<>();
+        Stack<ListNode> s2=new Stack<>();
         while(l1!=null)
         {
-            stk.push(l1);
+            s1.push(l1);
             l1=l1.next;
         }
         while(l2!=null)
         {
-            s.push(l2);
+            s2.push(l2);
             l2=l2.next;
         }
         int carry=0;
-        while(!stk.isEmpty() || !s.isEmpty() || carry!=0)
+        while(!s1.isEmpty() || !s2.isEmpty() || carry!=0)
         {
             int sum=carry;
-            if(!stk.isEmpty())
+            if(!s1.isEmpty())
             {
-                sum+=stk.pop().val;
+                sum+=s1.pop().val;
             }
-            if(!s.isEmpty())
+            if(!s2.isEmpty())
             {
-                sum+=s.pop().val;
+                sum+=s2.pop().val;
             }
             carry=sum/10;
             curr.next=new ListNode(sum%10);
