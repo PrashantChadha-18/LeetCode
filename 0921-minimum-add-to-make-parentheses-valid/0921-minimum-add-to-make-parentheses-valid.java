df28852870e8ac3c9a436628f95700e9f281ccd7
PrashantChadha-18 <prashantchadha58@gmail.com>
2026-08-02 17:27:20 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.39%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,25 +1,47 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> stk=new Stack<>();
-        int closing=0;
+        // Stack<Character> stk=new Stack<>();
+        // int closing=0;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if(ch=='(')
+        //     {
+        //         stk.push(ch);
+        //     }
+        //     else{
+        //         if(!stk.isEmpty())
+        //         {
+        //             stk.pop();
+        //         }
+        //         else
+        //         {
+        //             closing++;
+        //         }
+        //     }
+        // }
+        // return stk.size()+closing;
+        int open=0;
+        int close=0;
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
             if(ch=='(')
             {
-                stk.push(ch);
+                open++;
             }
-            else{
-                if(!stk.isEmpty())
+            else
+            {
+                if(open>0)
                 {
-                    stk.pop();
+                    open--;
                 }
                 else
                 {
-                    closing++;
+                    close++;
                 }
             }
         }
-        return stk.size()+closing;
+        return open+close;
     }
 }
