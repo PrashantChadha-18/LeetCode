133cc8da51ed09600b8f01dffa4b9ce3cbd285f0
PrashantChadha-18 <prashantchadha58@gmail.com>
2026-08-03 14:28:16 +0530

    Time: 3 ms (95.62%), Space: 44.3 MB (95.25%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,18 +1,33 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> stk=new Stack<>();
+        // Stack<Character> stk=new Stack<>();
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if(!stk.isEmpty() && (stk.peek()=='A' && ch=='B' || stk.peek()=='C' && ch=='D') )
+        //     {
+        //         stk.pop();
+        //     }
+        //     else
+        //     {
+        //         stk.push(ch);
+        //     }
+        // }
+        // return stk.size();
+
+        StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if(!stk.isEmpty() && (stk.peek()=='A' && ch=='B' || stk.peek()=='C' && ch=='D') )
-            {
-                stk.pop();
+            int n=sb.length();
+            if(n>0 && (sb.charAt(n-1)=='A' && ch=='B' || sb.charAt(n-1)=='C' && ch=='D')){
+                sb.deleteCharAt(n-1);
             }
             else
             {
-                stk.push(ch);
+                sb.append(ch);
             }
         }
-        return stk.size();
+        return sb.length();
     }
 }
