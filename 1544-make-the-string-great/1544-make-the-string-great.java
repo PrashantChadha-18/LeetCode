dbf851ba77b0ceccd090495105831fe49868e5d0
PrashantChadha-18 <prashantchadha58@gmail.com>
2026-08-03 14:08:29 +0530

    Time: 1 ms (100%), Space: 43.2 MB (92.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,23 +1,39 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stk=new Stack<>();
-        char[] ch=s.toCharArray();
-        for(int i=0;i<ch.length;i++)
+        // Stack<Character> stk=new Stack<>();
+        // char[] ch=s.toCharArray();
+        // for(int i=0;i<ch.length;i++)
+        // {
+        //     if(!stk.isEmpty() && Math.abs(stk.peek()-ch[i])==32)
+        //     {
+        //         stk.pop();
+        //     }
+        //     else
+        //     {
+        //         stk.push(ch[i]);
+        //     }
+        // }
+        // StringBuilder sb=new StringBuilder();
+        // while(!stk.isEmpty())
+        // {
+        //     sb.append(stk.pop());
+        // }
+        // return sb.reverse().toString();
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
         {
-            if(!stk.isEmpty() && Math.abs(stk.peek()-ch[i])==32)
+            char ch=s.charAt(i);
+            int n=sb.length();
+            if(n>0 && Math.abs(sb.charAt(n-1)-ch)==32)
             {
-                stk.pop();
+                sb.deleteCharAt(n-1);
             }
             else
             {
-                stk.push(ch[i]);
+                sb.append(ch);
             }
         }
-        StringBuilder sb=new StringBuilder();
-        while(!stk.isEmpty())
-        {
-            sb.append(stk.pop());
-        }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
